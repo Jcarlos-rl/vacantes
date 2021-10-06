03c8b68dd56c52c5a1dded5ec7a2bc03c9410625
Juan Carlos Romero Lorenzo <juanromerolorenzo@MacBook-Air-de-Juan.local>
2021-10-06 11:34:11 -0500

    Add styles forms

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,27 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
+    <h3 class="text-center text-primary fw-bold">Confirma tú email</h3>
+    <div class="col-lg-8 offset-lg-2 mt-3">
+        @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('A fresh verification link has been sent to your email address.') }}
             </div>
-        </div>
+        @endif
+
+        {{ __('Before proceeding, please check your email for a verification link.') }}
+        {{ __('If you did not receive the email') }},
+        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+        </form>
     </div>
 </div>
 @endsection
