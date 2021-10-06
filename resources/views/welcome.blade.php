4e94dd2dac2efc6a54c6bc07e19a2112f6030405
Juan Carlos Romero Lorenzo <juanromerolorenzo@MacBook-Air-de-Juan.local>
2021-10-06 11:50:06 -0500

    Prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h1 class="text-center">Vacantes Home</h1>
+    <p>Laborum aute veniam sit reprehenderit id irure Lorem aute laborum. Aliquip consequat adipisicing excepteur ea in do officia elit amet nostrud consequat. Reprehenderit ut do sunt tempor reprehenderit.</p>
 @endsection
